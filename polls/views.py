8b68117eab21d7808244bd2ef87e8b7de41735d2
Kyle Ortiz <kyleaortiz@gmail.com>
2022-02-15 23:45:57 -0600

    added more views

--- polls/views.py
+++ polls/views.py
@@ -8,3 +8,13 @@ def index(request):
 
 def create(request):
     return HttpResponse("Hello, world. You're at the polls create.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
